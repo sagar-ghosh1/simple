41887baeef876b9e210f23ba466da79c5241c429
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-20 03:32:40 +0600

    error

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../shared/NavBar/NavBar";
 import Footer from "../shared/Footer/Footer";
+import NotFound from "../pages/NotFound/NotFound";
 
 const Main = () => {
    return (
       <div>
-         <NavBar></NavBar>
+         {/* <NavBar></NavBar> */}
          <Outlet></Outlet>
-         <Footer></Footer>
+         {/* <Footer></Footer> */}
+         <NotFound></NotFound>
       </div>
    );
 };
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const NotFound = () => {
    return (
-      <div>
-
+      <div className="hero min-h-screen bg-base-200">
+         <div className="hero-content text-center">
+            <div className="max-w-md">
+               <div className='mb-8'>
+                  <img src="/src/assets/notFound.jpg" alt="" />
+               </div>
+               <h1 className="text-5xl font-bold">Oops! <br />Page not found</h1>
+               <p className="py-6">The page you are looking for could not be found.</p>
+               <button className="btn btn-primary">Back to Home</button>
+            </div>
+         </div>
       </div>
    );
 };
