955428d663ea5518057fb7d0a357c5e549d5361b
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-20 01:27:25 +0600

    footer 2

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import { FaMapMarkerAlt } from 'react-icons/fa';
+import { FaFacebook, FaGoogle, FaInstagram, FaLinkedin, FaMapMarkerAlt, FaMobileAlt, FaYoutube } from 'react-icons/fa';
 
 
 const Footer = () => {
   return (
     <footer className="footer text-white p-20 bg-[#143338]">
-      <div>
+      <div className='gap-3'>
         <img className="h-20" src="/src/assets/images.png" alt="" />
         <h1 className="text-2xl font-bold">Magical Toy</h1>
 
@@ -13,8 +13,16 @@ const Footer = () => {
           <p><FaMapMarkerAlt></FaMapMarkerAlt></p>
           <p>Uttra, Dhaka, Bangladesh</p>
         </div>
+        <div className='flex items-center'>
+          <p><FaMobileAlt></FaMobileAlt></p>
+          <p>Uttra, Dhaka, Bangladesh</p>
+        </div>
+        <div className='flex items-center'>
+          <p><FaMobileAlt></FaMobileAlt></p>
+          <p>magical@toy.com</p>
+        </div>
       </div>
-      <div>
+      <div className='gap-7'>
         <span className="footer-title">Services</span>
         <a className="link link-hover">My account</a>
         <a className="link link-hover">Login</a>
@@ -22,7 +30,7 @@ const Footer = () => {
         <a className="link link-hover">Promotional Offer</a>
         <a className="link link-hover">Order tracking</a>
       </div>
-      <div>
+      <div className='gap-7'>
         <span className="footer-title">Company</span>
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
@@ -30,7 +38,7 @@ const Footer = () => {
         <a className="link link-hover">All Products</a>
         <a className="link link-hover">Locations Map</a>
       </div>
-      <div>
+      <div className='gap-7'>
         <span className="footer-title">Ouick Links</span>
         <a className="link link-hover">Support</a>
         <a className="link link-hover">Helpline</a>
@@ -39,17 +47,27 @@ const Footer = () => {
         <a className="link link-hover">Event</a>
       </div>
 
-      <div>
-        <span className="footer-title">Newsletter</span>
+      <div className='gap-6'>
+        <h1 className="footer-title">Newsletter</h1>
         <div className="form-control w-80">
-          <label className="label">
-            <span className="label-text">Enter your email address</span>
-          </label>
+          <h5>Through this email you can communicate very well without any problem.</h5><br />
           <div className="relative">
             <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
             <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
           </div>
         </div>
+
+        <div>
+          <h5 className='text-xl'>All Social Site</h5>
+          <div className='flex gap-3 text-xl mt-3'>
+            <a href=""><FaGoogle></FaGoogle></a>
+            <a href=""><FaFacebook></FaFacebook></a>
+            <a href=""><FaYoutube></FaYoutube></a>
+            <a href=""><FaInstagram></FaInstagram></a>
+            <a href=""><FaLinkedin></FaLinkedin></a>
+          </div>
+        </div>
+
       </div>
     </footer>
   );
