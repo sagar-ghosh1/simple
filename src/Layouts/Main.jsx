bcdb4233fcb9d96663bd228dcea5e8482a465942
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-20 04:12:44 +0600

    banner section

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -2,14 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../shared/NavBar/NavBar";
 import Footer from "../shared/Footer/Footer";
 import NotFound from "../pages/NotFound/NotFound";
+import Banner from "../pages/Home/Banner/Banner";
 
 const Main = () => {
    return (
       <div>
          {/* <NavBar></NavBar> */}
          <Outlet></Outlet>
+         <Banner></Banner>
          {/* <Footer></Footer> */}
-         <NotFound></NotFound>
+         {/* <NotFound></NotFound> */}
       </div>
    );
 };
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const Banner = () => {
-   return (
-      <div>
-         
+  return (
+    <div>
+      <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.pexels.com/photos/3661243/pexels-photo-3661243.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")` }}>
+        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <img src="https://demo.ishithemes.com/shopify/SHOPY107/asset/img/logo.png" alt="" />
+            <h1 className="mb-5 text-6xl font-bold">Best Quality Toys</h1>
+            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">LEARN MORE</button>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Banner;
